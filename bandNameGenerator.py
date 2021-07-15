c4976f8f57464769e3d2f062be727819b54884df
Sakshi Kedar <52277353+sakshikedar@users.noreply.github.com>
2021-07-15 22:22:58 +0530

    Add files via upload

---
+++ bandNameGenerator.py
@@ -0,0 +1,9 @@
+#print("hellow world \nhello world")
+#print("hello "+"angela")
+
+
+
+print("Welcome to band name generator: ")
+a= input("What is your favorite animal? ")
+b= input("what is your favorite color? ")
+print("Your band name could be: " + a + b)
